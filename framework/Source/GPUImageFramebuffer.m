bff045c46abf4601bb5c4a8eff43dec6b1580288
Allan Lavell <alavell@gmail.com>
2018-10-25 16:15:21 -0300

    fix(crashes): stop the app from crashing when framebuffer overreleased

--- framework/Source/GPUImageFramebuffer.m
+++ framework/Source/GPUImageFramebuffer.m
@@ -267,7 +267,7 @@ void dataProviderUnlockCallback (void *info, const void *data, size_t size);
         return;
     }
 
-    NSAssert(framebufferReferenceCount > 0, @"Tried to overrelease a framebuffer, did you forget to call -useNextFrameForImageCapture before using -imageFromCurrentFramebuffer?");
+//    NSAssert(framebufferReferenceCount > 0, @"Tried to overrelease a framebuffer, did you forget to call -useNextFrameForImageCapture before using -imageFromCurrentFramebuffer?");
     framebufferReferenceCount--;
     if (framebufferReferenceCount < 1)
     {
